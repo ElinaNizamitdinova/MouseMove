cfd07bd20ff5b52f739bdecc363a1affbc3eb7ca
Elina <elina.eldarovna.spb@gmail.com>
2025-02-10 14:09:27 +0300

    Add MouseMovementDomain

---
+++ MouseMovementDomain/Domain/MouseDataDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseMovementDomain.Domain
+{
+    public class MouseDataDto
+    {
+        public List<long[]> Coordinates { get; set; }
+    }
+}
