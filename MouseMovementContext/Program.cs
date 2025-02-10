552ff0b169166a00ae408f4c1fc51cd63674d67b
Elina <elina.eldarovna.spb@gmail.com>
2025-02-10 14:01:24 +0300

    first commit

---
+++ MouseMovementContext/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MouseMovementContext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
